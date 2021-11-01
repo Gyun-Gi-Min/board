2d5d0934f0a02e08fc0ebe1a1518c24ecac64cc1
Gyun-Gi-Min <mingg95@gmail.com>
2021-11-02 07:56:13 +0900

    1029

---
+++ src/main/java/com/koreait/server/DelBoardServlet.java
@@ -0,0 +1,25 @@
+package com.koreait.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/del")
+public class DelBoardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String pIboard = req.getParameter("iboard");
+        System.out.println("pIboard : "+ pIboard);
+
+        int iboard = Integer.parseInt(pIboard);
+        //pIboard를 정수로 전환.
+        BoardVO param = new BoardVO();
+        param.setIboard(iboard);
+        BoardDAO.delBoard(param);
+
+    }
+
+}
---
+++ src/main/java/com/koreait/server/updBoardServlet.java
@@ -0,0 +1,25 @@
+package com.koreait.server;
+
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/upd")
+public class updBoardServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        String json = Utils.getJson(req);
+        Gson gson = new Gson();
+        BoardVO param = gson.fromJson(json,BoardVO.class);
+        int result = BoardDAO.updBoard(param);
+        System.out.println("result : "+result);
+
+    }
+}
